083298a072ec16c4b1d9c7c35c78282eef306b6e
bruno <brunofigueroa2290@gmail.com>
2024-10-02 22:48:52 -0300

    nuevo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,43 +6,39 @@ const Header = () => {
          
   
           <nav className="navbar navbar-expand-lg bg-success">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar scroll</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarScroll">
-        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
-        <li className="nav-item">
+            <div className="container-fluid">
+             <a className="navbar-brand" href="#">LA LIGA</a>
+                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                 </button>
+             <div className="collapse navbar-collapse" id="navbarScroll">
+                 <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
+                  <li className="nav-item">
             <a className="nav-link" href="Home">Home</a>
-          </li>
-          <li className="nav-item">
+                  </li>
+                 <li className="nav-item">
             <a className="nav-link" href="Programas">Programas</a>
-          </li>
-          <li className="nav-item">
+                 </li>
+                 <li className="nav-item">
             <a className="nav-link" href="Posiciones">Posiciones</a>
-          </li>
-          <li className="nav-item">
+                 </li>
+                  <li className="nav-item">
             <a className="nav-link" href="#">Resultados</a>
-          </li>
-          <li className="nav-item">
+                 </li>
+                 <li className="nav-item">
             <a className="nav-link" href="#">Canchas</a>
-          </li>
-          <li className="nav-item">
+                 </li>
+                 <li className="nav-item">
             <a className="nav-link" href="#">Reglamento</a>
-          </li>
-          <li className="nav-item">
+                 </li>
+                 <li className="nav-item">
             <a className="nav-link" href="#">Boletines</a>
+                </li>
+                <li className="nav-item">
+            <a className="nav-link" href="#">Contacto</a>
           </li>
           
-          <li className="nav-item">
-            <a className="nav-link" href="#">Ligas de futbol</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Fixture</a>
-          </li>
           
-  
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Ligas de futbol
@@ -52,9 +48,7 @@ const Header = () => {
            
             </ul>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" aria-disabled="true">Contacto</a>
-          </li>
+          
         </ul>
         
       </div>
