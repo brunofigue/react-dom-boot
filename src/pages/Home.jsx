d133499996a026f24af9d7f0c64af197d7539400
bruno <brunofigueroa2290@gmail.com>
2024-10-07 18:42:25 -0300

    nuevo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,20 @@ const Home = () => {
   return (
     <main className="d-flex h-custom">
         <section className="text d-flex justify-content-center align-items-center">
-            <h1 className="m-auto text-size-custom"></h1>
+            <h1 className="m-auto"></h1>
         </section>
         <figure className="container-img d-flex justify-content-center">
             <img className="banner" src="https://img.freepik.com/foto-gratis/concepto-collage-html-css-persona_23-2150061986.jpg" alt="logo" />
         </figure>
-        <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
-      </div>
+        
       <section>
         <h2> Bruno Figueroa</h2>
         <p className="txt">
-        Buenas, mi nombre es Bruno Figueroa, tengo 34 años y resido en Argentina. Cuento con conocimientos en desarrollo web, en base de datos, metodologías ágiles, diferentes tecnologías y Cloud Computing. Soy una persona muy comprometida, comunicativa, proactiva y empática con ánimos de emprender nuevos desafíos. En constante capacitación sobre las nuevas herramientas y tecnológicas actuales.
+        Buenas, mi nombre es Bruno Figueroa, tengo 34 años y resido en Argentina. 
+        Cuento con conocimientos en desarrollo web, en base de datos, metodologías ágiles, 
+        diferentes tecnologías y Cloud Computing. Soy una persona muy comprometida, comunicativa, 
+        proactiva y empática con ánimos de emprender nuevos desafíos. En constante capacitación sobre las 
+        nuevas herramientas y tecnológicas actuales.
         </p>
       </section>
    
