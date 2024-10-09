019daa9d4db8ec1f5389f773ed3fb34f9fef07c3
bruno <brunofigueroa2290@gmail.com>
2024-10-09 16:30:21 -0300

    redireccionar

--- src/pages/Home/components/Projects/Projects.jsx
+++ src/pages/Home/components/Projects/Projects.jsx
@@ -24,7 +24,7 @@ function Projects() {
           <p className='card-text'>
           Proyecto realizado para entrega 10/09/24
           </p>
-          <a href='#' className='btn btn-primary'>
+          <a href='https://github.com/brunofigue/entrega-con-js.git' className='btn btn-primary'>
           Ver proyecto a github
           </a>
         </div>
@@ -32,13 +32,15 @@ function Projects() {
       <div className='card' style={{ width: "18rem" }}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg1MndL-Xp1JcnqaB0YOqTp6zDjrwYyGKsPA&s' alt='...' />
         <div className='card-body'>
-          <h5 className='card-title'>React</h5>
-          <p className='card-text'>
-          Proyecto realizado para entrega 20/10/24
-          </p>
-          <a href='#' className='btn btn-primary'>
-          Ver proyecto a github
-          </a>
+          <br />
+          <br />
+            <h5 className='card-title'>React</h5>
+            <p className='card-text'>
+            Proyecto realizado para entrega 20/10/24
+            </p>
+            <a href='https://github.com/brunofigue/react-dom-boot.git' className='btn btn-primary'>
+            Ver proyecto a github
+            </a>
         </div>
       </div>
     </section>
